2ec90bb7757d01a14b617c902e77b069a75954a3
johnny lamb <agnusjack@gmail.com>
2015-09-14 18:35:25 -0600

    added delete and edit buttons

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -2,14 +2,41 @@ $(document).on('ready', function() {
   console.log('sanity check!');
 });
 
-$('form').on("submit",function(e){
-e.preventDefault();
+$('form').on("submit", function(e) {
+  e.preventDefault();
 
-var payload = {
-  name: $('#name').val(),
-  age: $('#age').val(),
-  spitter:$('#spitter').val()
+  var payload = {
+    name: $('#name').val(),
+    age: $('#age').val(),
+    spitter: spitter
+  };
+  if ($('#spitter').is(':checked')) {
+    payload.spitter = true;
+  } else {
 
-};
-  console.log(payload);
+    payload.spitter = false;
+  }
+  $.post('/api/lamas', payload, function(lamas) {
+    $('.message-section').show();
+    $('#message').html('Added ' + lamas.name + " " + lamas.age + lamas.spitter);
+    console.log(lamas);
+    getLamas();
+  });
+
 });
+
+function getLamas() {
+  $('#all-lamas').html('');
+  $.get('/api/lamas', function(lama) {
+    for(var i = 0; i < lama.length; i++) {
+        $('#all-lamas').prepend('<tr>' +
+        '<td>' + lama[i].name + '</td>' +
+        '<td>' + lama[i].age + '</td>' + lama[i].spitter + '</td>'
+         );
+        $('form input').val('');
+        $('#spitter').removeAttr('checked');
+    }
+  });
+}
+
+
